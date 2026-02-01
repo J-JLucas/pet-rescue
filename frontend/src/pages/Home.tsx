ceb34c2fde5dd6c3e5e232840dd572629068c7ea
JoSh <jlucas205@proton.me>
2026-02-01 00:42:52 -0700

    header tweaks, header now floats static in place

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -8,10 +8,9 @@ export function Home() {
       <div className='home-card'>
         <h1 className='subheading'>You Can Make a Difference!</h1>
         <div home-card-container>
-
+          <h1 className='subheading'>Our Mission</h1>
+          <p className='mission-body'>Our mission is to give every cat the chance to feel safe, loved, and truly at home. We rescue cats in need, provide compassionate care, and match them with families who are ready to love them for life.</p>
         </div>
-        <h1 className='subheading'>Our Mission</h1>
-        <p className='mission-body'>Our mission is to give every cat the chance to feel safe, loved, and truly at home. We rescue cats in need, provide compassionate care, and match them with families who are ready to love them for life.</p>
       </div>
     </Layout>
   );
