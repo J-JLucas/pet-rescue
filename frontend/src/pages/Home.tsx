984e9e37fb5e2a4ce1cefe83138d1a828f524663
JoSh <jlucas205@proton.me>
2026-01-31 17:29:07 -0700

    added body to homepage

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -7,7 +7,11 @@ export function Home() {
       <img src="catcouple.jpg" alt='Two cute cats cuddling' className='hero-image' />
       <div className='home-card'>
         <h1 className='subheading'>You Can Make a Difference!</h1>
-        <p></p>
+        <div home-card-container>
+
+        </div>
+        <h1 className='subheading'>Our Mission</h1>
+        <p className='mission-body'>Our mission is to give every cat the chance to feel safe, loved, and truly at home. We rescue cats in need, provide compassionate care, and match them with families who are ready to love them for life.</p>
       </div>
     </Layout>
   );
